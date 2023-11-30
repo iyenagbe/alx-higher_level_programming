99c292406b5a4f8db0a9f3b30e5d92bb37f8a5fc
iyenagbe <iyenagberaymond@gmail.com>
2023-11-30 09:29:40 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,7 +2,6 @@
 # 102-magic_calculation.py
 # Brennan D Baraban <375@holbertonschool.com>
 
-
 def magic_calculation(a, b):
     """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+# 4-hidden_discovery.py
+# Brennan D Baraban <375@holbertonschool.com>
+
 if __name__ == "__main__":
     import sys
     for content in dir(sys):
