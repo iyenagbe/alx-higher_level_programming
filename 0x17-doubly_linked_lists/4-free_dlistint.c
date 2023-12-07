773288832c785d7c5ed1e3a48233f0602294ec4d
iyenagbe <iyenagberaymond@gmail.com>
2023-12-07 04:24:06 -0800

    0x17-doubly_linked_lists

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+* free_dlistint - function
+* @head: pointer to first node of dbl lnkd list
+* Decription: function to free a  dbl lnkd list
+* Return: 0 (updated)
+*/
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temph = head;
+
+	if (head == NULL)
+		return;
+
+	while (head->next != NULL)
+	{
+		temph = head;
+		head = head->next;
+		free(temph);
+	}
+	free(head);
+}
---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* sum_dlistint - function
+* @head: pointer to first node in dbl lnkd list
+* Description: function to return sum of all dat (n) in dbl list
+* Return: The sum of data or 0
+*/
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *temph;
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	temph = head;
+	while (temph != NULL)
+	{
+		sum += temph->n;
+		temph = temph->next;
+	}
+	return (sum);
+}
