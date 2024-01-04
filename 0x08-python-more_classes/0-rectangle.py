d6430f19a3be98330fce7f8bbc58bba152bc768f
iyenagbe <iyenagberaymond@gmail.com>
2024-01-04 08:28:26 -0800

    python is cool

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
+
