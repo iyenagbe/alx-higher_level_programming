9c81b18c184f9f275805ed28a5cbcc98aae2732b
iyenagbe <iyenagberaymond@gmail.com>
2024-01-05 07:28:53 -0800

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Module for lockedclass"""
+"""Defines a locked class."""
+
 
 class LockedClass:
-    """class LockedClass"""
-    def __init__(self):
-        self.lock = threading.Lock()
-        Lock.acquire(True)
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
