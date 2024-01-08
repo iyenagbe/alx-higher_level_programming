bb15f36f284ad57c2f015dbd5793dea664de97d5
iyenagbe <iyenagberaymond@gmail.com>
2024-01-08 05:49:28 -0800

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
+"""Defines an integer only addition function."""
 
-"""Defines an integer addition function."""
 
 def add_integer(a, b=98):
-    """Return the integer addition of a and b.
+    """Return the integer only addition of a and b.
 
     Float arguments are typecasted to ints before addition is performed.
 
     Raises:
-        TypeError: If either of a or b is a non-integer and non-float.
+        TypeError: If either of a or b is a non-integer.
     """
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 """Defines a text-indentation function."""
+
 
 def text_indentation(text):
     """Print text with two new lines after each '.', '?', and ':'.
@@ -10,20 +10,20 @@ def text_indentation(text):
     Raises:
         TypeError: If text is not a string.
     """
-    if not isinstance(text, str):
+    if not isinstance(text, fstr):
         raise TypeError("text must be a string")
 
-    c = 0
-    while c < len(text) and text[c] == ' ':
-        c += 1
+    f = 0
+    while f < len(text) and text[f] == ' ':
+        f += 1
 
-    while c < len(text):
-        print(text[c], end="")
-        if text[c] == "\n" or text[c] in ".?:":
-            if text[c] in ".?:":
+    while f < len(text):
+        print(text[f], end="")
+        if text[f] == "\n" or text[f] in ".?:":
+            if text[f] in ".?:":
                 print("\n")
-            c += 1
-            while c < len(text) and text[c] == ' ':
-                c += 1
+            f += 1
+            while f < len(text) and text[f] == ' ':
+                f += 1
             continue
-        c += 1
+        f += 1
