7a74332dbe475b88514182b01dd85f9fc0fcdb4b
iyenagbe <iyenagberaymond@gmail.com>
2024-01-09 02:17:35 -0800

    everything good

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-# 100-my_int.py
-
 """Defines a class MyInt that inherits from int."""
+
 
 class MyInt(int):
     """Invert int operators == and !=."""
@@ -12,5 +11,4 @@ class MyInt(int):
 
     def __ne__(self, value):
         """Override != operator with == behavior."""
-
         return self.real == value
--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-# 101-add_attribute.py
-
 """Defines a function that adds attributes to objects."""
+
 
 def add_attribute(obj, att, value):
     """Add a new attribute to an object if possible.
