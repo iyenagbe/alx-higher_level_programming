5bde22f910ed51070b0e4381022212050b96420e
iyenagbe <iyenagberaymond@gmail.com>
2024-02-13 05:09:03 -0800

    Structured Query Language

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- Inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+-- Inserts a new row into the table first_table in my MySQL server.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
